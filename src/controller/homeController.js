5965d8bea00c92812121ac6324b259c20aac0f9e
Rykrax1122 <rykrax.ksnd.1122@gmail.com>
2025-05-02 22:57:11 +0700

    feat: load track list

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,20 @@
+import fetch from 'node-fetch'; // Ensure you have node-fetch installed
+
+// const getHomepage = (req, res) => {
+//     fetch("http://localhost:8080/api/get-tracks")
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log("Data from API:", data);
+//             res.render('main.ejs', { title: 'Test Page', tracks: data });
+//         })
+//         .catch(error => {
+//             console.error("Error fetching data:", error);
+//             res.status(500).send("Error fetching data");
+//         });
+// };
+
 const getHomepage = (req, res) => {
-    res.render('main.ejs', { title: 'Test Page' });
+    res.render('main.ejs');
 };
 
 export {
